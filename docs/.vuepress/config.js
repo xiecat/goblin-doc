43c6e36e9e74fcff0f0bdeb99d9149c2c27f8fea
becivells <732903873@qq.com>
2021-09-12 09:10:49 +0800

    add doc

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'goblin',
+    description: 'goblin 是一款适用于红蓝对抗中的蜜罐和钓鱼系统',
+    themeConfig: {
+        nav:[ // 导航栏配置
+            {text: 'github', link: 'https://github.com/xiecat/goblin'}
+        ],
+        sidebar: 'auto', // 侧边栏配置
+        sidebarDepth: 2, // 侧边栏显示2级
+    }
+}
